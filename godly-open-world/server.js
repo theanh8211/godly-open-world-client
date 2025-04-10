b0835a351141a32562969a823af6b98a90a6af66
theanh8211 <trantheanh.12a3@gmail.com>
2025-04-10 08:04:20 +0700

    fix(ws): allow external access

--- godly-open-world/server.js
+++ godly-open-world/server.js
@@ -1,7 +1,13 @@
+const http = require('http');
+const server = http.createServer();
 const WebSocket = require('ws');
-const fs = require('fs');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ server });
+
+let PORT = process.env.PORT || 8080;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on ws://0.0.0.0:${PORT}`);
+});
 let users = {};
 let onlinePlayers = {};
 let tiles = [];
@@ -201,4 +207,4 @@ wss.on('connection', (ws) => {
     });
 });
 
-console.log('Server running on ws://localhost:8080');
+console.log('Server running on ws://0.0.0.0:8080');
